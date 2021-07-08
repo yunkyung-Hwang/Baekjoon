c701c6c0f792f999e7e8f297c85e5fd4e2a2f40b
yunkyung-Hwang <61379671+yunkyung-Hwang@users.noreply.github.com>
2021-07-08 13:42:50 +0900

    Create 5567_결혼식(실패).swift

---
+++ 5567_결혼식(실패).swift
@@ -0,0 +1,34 @@
+import Foundation
+
+let n = Int(readLine()!)!
+let m = Int(readLine()!)!
+
+var friends : [[Int]] = Array(repeating: Array(repeating: 0, count: 2), count: m)
+var isFriend = [Bool](repeating: false, count: n)   // 상근이 친구
+var isFriendsFriend = [Bool](repeating: false, count: n)    // 상근이 친구의 친구
+var cnt = 0
+
+for i in 0..<friends.count {    // 입력
+    friends[i] = (readLine()!.split(separator: " ")).map{ Int($0)! }
+}
+//print(friends)
+
+for i in 0..<friends.count {
+    if friends[i][0] == 1 { // 상근이 친구 찾기
+        isFriend[friends[i][1] - 1] = true
+    }
+}
+
+for i in 0..<friends.count {
+    if friends[i][0] != 1 && isFriend[friends[i][0] - 1] {  // 친구의 친구 찾기
+        isFriendsFriend[friends[i][1] - 1] = true
+    }
+}
+
+for i in 0..<isFriend.count {
+    //print(isFriend[i],isFriendsFriend[i])
+    if isFriend[i] || isFriendsFriend[i]{
+        cnt += 1
+    }
+}
+print(cnt)
