6097b11f498991b17ce74df7523ae275c39a8a1e
yunkyung-Hwang <61379671+yunkyung-Hwang@users.noreply.github.com>
2021-07-10 17:31:43 +0900

    Create 15787_기차가 어둠을 헤치고 은하수를(시간초과).swift

---
+++ 15787_기차가 어둠을 헤치고 은하수를(시간초과).swift
@@ -0,0 +1,44 @@
+import Foundation
+
+let nm = (readLine()!.split(separator: " ")).map{ Int($0)! }
+let n = nm[0]
+let m = nm[1]
+
+var trains :[[Bool]] = Array(repeating: Array(repeating: false, count: 20), count: n)
+
+for _ in 0..<m{
+    let cmd = readLine()!.components(separatedBy: " ")
+    let trainNum = Int(cmd[1])! - 1
+
+    if cmd[0] == "1" {
+        let seatNum = Int(cmd[2])! - 1
+        trains[trainNum][seatNum] = true
+    } else if cmd[0] == "2" {
+        let seatNum = Int(cmd[2])! - 1
+        trains[trainNum][seatNum] = false
+    } else if cmd[0] == "3" {
+        trains[trainNum].removeLast()
+        trains[trainNum].insert(false, at: 0)
+    } else if cmd[0] == "4" {
+        trains[trainNum].removeFirst()
+        trains[trainNum].append(false)
+    }
+}
+
+var cnt = 0
+var isNewTrain = [Bool](repeating: true, count: n)
+for i in 0..<n-1 {
+    for j in i+1..<n {
+        if trains[i].elementsEqual(trains[j]) {
+            isNewTrain[j] = false
+        }
+    }
+}
+
+for i in isNewTrain {
+    if i {
+        cnt += 1
+    }
+}
+
+print(cnt)
