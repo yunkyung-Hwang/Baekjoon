492b058f8d11cb97bc1182305ebdb839c27f87de
yunkyung-Hwang <61379671+yunkyung-Hwang@users.noreply.github.com>
2022-09-01 16:41:40 +0900

    Update 6603_로또.swift

--- 6603_로또.swift
+++ 6603_로또.swift
@@ -1,35 +1,26 @@
 import Foundation
 
-var test = readLine()!.split(separator: " ").map { Int($0)! }
-var visit = [Bool]()
-var result = [Int]()
-
-while test[0] != 0 {
-    let k = test[0]
-    for _ in 0..<k {
-        visit.append(false)
-    }
-    test.removeFirst()
-    comb(r: 0, n: 0)
+while true {
+    let test = readLine()!.split(separator: " ").map { Int($0)! }
+    if test[0] == 0 { break }
     
+    var visit = [Bool](repeating: false, count: test[0])
+    var result = [Int]()
+    
+    comb(cnt: 0, start: 0)
     print()
-    
-    test.removeAll()
-    visit.removeAll()
-    test = readLine()!.split(separator: " ").map { Int($0)! }
-}
 
-func comb(r: Int, n: Int) {
-    if r == 6 {
-        print(result.map { String($0) }.joined(separator: " "))
-    }
-    
-    for i in n..<test.count {
-        if visit[i] { continue }
-        visit[i] = true
-        result.append(test[i])
-        comb(r: r+1, n: i)
-        result.removeLast()
-        visit[i] = false
+    func comb(cnt: Int, start: Int) {
+        if cnt == 6 { print(result.map { String($0)}.joined(separator: " ")) }
+        else {
+            for i in start..<test[0] {
+                if visit[i] { continue }
+                visit[i] = true
+                result.append(test[i + 1])
+                comb(cnt: cnt+1, start: i)
+                result.removeLast()
+                visit[i] = false
+            }
+        }
     }
 }
