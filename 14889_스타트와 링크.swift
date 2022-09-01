80ed8e109a73e5fd8ad245eb4b1746236a15e437
yunkyung-Hwang <61379671+yunkyung-Hwang@users.noreply.github.com>
2022-09-01 16:11:01 +0900

    Update 14889_스타트와 링크.swift

--- 14889_스타트와 링크.swift
+++ 14889_스타트와 링크.swift
@@ -1,38 +1,41 @@
 import Foundation
 
 let n = Int(readLine()!)!
-var s = [[Int]]()
+var s = [[Int]](repeating: [Int](repeating: 0, count: n), count: n)
 var visit = [Bool](repeating: false, count: n)
 var result = Int.max
 
-for _ in 0..<n {
-    s.append(readLine()!.split(separator: " ").map{ Int(String($0))! })
+for i in 0..<n {
+    let tmp = readLine()!.split(separator: " ").map { Int($0)! }
+    s[i] = tmp
 }
-dfs(idx: 0, cnt: 0)
+
+dfs(cnt: 0, idx: 0)
 print(result)
 
-func dfs(idx: Int, cnt: Int) {
+func dfs(cnt: Int, idx: Int) {
     if cnt == n / 2 {
-        calculate()
-        return
-    }
-    
-    for i in idx..<n {
-        if !visit[i] {
+        result = min(result, calculate())
+        if result == 0 {
+            print(0)
+            exit(0)
+        }
+    } else {
+        for i in idx..<n {
+            if visit[i] { continue }
             visit[i] = true
-            dfs(idx: i + 1, cnt: cnt + 1)
+            dfs(cnt: cnt+1, idx: i+1)
             visit[i] = false
         }
     }
 }
 
-// 능력치 
-func calculate() {
+func calculate() -> Int {
     var start = 0
     var link = 0
     
     for i in 0..<n-1 {
-        for j in i+1..<n {
+        for j in i..<n {
             if visit[i] && visit[j] {
                 start += s[i][j]
                 start += s[j][i]
@@ -43,12 +46,5 @@ func calculate() {
         }
     }
     
-    let tmp = abs(start - link)
-    
-    if tmp == 0 {
-        print(tmp)
-        exit(0)
-    }
-    
-    result = min(result, tmp)
+    return abs(start - link)
 }
