bec0f5553135dba5ed32503c49e239d94fdeab89
yunkyung-Hwang <61379671+yunkyung-Hwang@users.noreply.github.com>
2022-09-06 23:20:32 +0900

    bfs_시간초과

--- 9019_DSLR.swift
+++ 9019_DSLR.swift
@@ -1,7 +1,10 @@
+// 시간초과
+// String에서 int로 바꿔도 
+
 import Foundation
 
 var t = Int(readLine()!)!
-let dslr = ["D", "S", "L", "R"]
+let dslr = ["", "D", "S", "L", "R"]
 
 while t > 0 {
     let ab = readLine()!.split(separator: " ").map { Int($0)! }
@@ -14,41 +17,47 @@ while t > 0 {
 }
 
 func bfs(a: Int, b: Int) {
-    var queue = Queue<[String]>()
+    var queue = Queue<[Int]>()
     var visit = [Bool](repeating: false, count: 10000)
     
-    queue.push([String(a), ""])
+    queue.push([a, 0])
     visit[a] = true
     
     while !queue.isEmpty {
         let tmp = queue.pop()!
-        let num = Int(tmp[0])!
-        let result = tmp[1]
+        let num = tmp[0]
+        let path = tmp[1]
         if num == b {
+            var tmp = path
+            var result = ""
+            while tmp > 0 {
+                result += dslr[tmp%10]
+                tmp /= 10
+            }
             print(result)
             return
         }
         
         // dslr
         var dslrNum = 0
-        for i in 0..<4 {
+        for i in 1..<5 {
             dslrNum = dslr(i, num)
             if !visit[dslrNum] {
                 visit[dslrNum] = true
-                queue.push([String(dslrNum), result + dslr[i]])
+                queue.push([dslrNum, path * 10 + i])
             }
         }
     }
 }
 
 func dslr(_ idx: Int, _ num: Int) -> Int {
-    if idx == 0 {
+    if idx == 1 {
         return num * 2 % 10000
-    } else if idx == 1 {
-        return num == 0 ? 9999 : num - 1
     } else if idx == 2 {
-        return num % 1000 * 10 + num / 1000
+        return num == 0 ? 9999 : num - 1
     } else if idx == 3 {
+        return num % 1000 * 10 + num / 1000
+    } else if idx == 4 {
         return num % 10 * 1000 + num / 10
     }
     return 0
