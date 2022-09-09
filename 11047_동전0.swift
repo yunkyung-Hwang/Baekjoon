4131d07aed8602a20745a405ac9ff1202947ecf2
yunkyung-Hwang <61379671+yunkyung-Hwang@users.noreply.github.com>
2022-09-09 19:33:30 +0900

    그리디

--- 11047_동전0.swift
+++ 11047_동전0.swift
@@ -1,28 +1,23 @@
 import Foundation
 
-let NK = readLine()!
-let NKArr = NK.components(separatedBy: " ")
-let n = Int(NKArr[0])!
-var k = Int(NKArr[1])!
-
+let nk = readLine()!.split(separator: " ").map{ Int($0)! }
+let n = nk[0]
+var k = nk[1]
 var coin = [Int](repeating: 0, count: n)
-var tmp: Int
 var result = 0
-
 
 for i in 0..<n {
     coin[i] = Int(readLine()!)!
 }
 
-while k > 0 {  
-    
-    for i in 0..<n {
-        if k / coin[coin.count-1-i] == 0 {
-            continue
-        }
-        result += k / coin[coin.count-1-i]
-        k -= k / coin[coin.count-1-i] * coin[coin.count-1-i]
+var idx = n - 1
+while true {
+    while coin[idx] <= k {
+        k -= coin[idx]
+        result += 1
     }
-   
+    if k == 0 { break }
+    idx -= 1
 }
+
 print(result)
