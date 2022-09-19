50aa3e0a3d120800ee7d3e6733a6f2a5442ceb28
yunkyung-Hwang <61379671+yunkyung-Hwang@users.noreply.github.com>
2022-09-19 23:10:09 +0900

    ver 2

--- 11729_하노이 탑 이동 순서.swift
+++ 11729_하노이 탑 이동 순서.swift
@@ -19,3 +19,27 @@ func hanoi(from: Int, to: Int, tmp: Int, cnt: Int) {
         hanoi(from: tmp, to: to, tmp: from, cnt: cnt - 1)
     }
 }
+
+// ver 2, tmp 없이!
+// import Foundation
+
+// let n = Int(readLine()!)!
+// var move = [String]()
+// var result = 0
+
+// hanoi(from: 1, to: 3, cnt: n)
+
+// print(result)
+// move.forEach {
+//     print($0)
+// }
+
+// func hanoi(from: Int, to: Int, cnt: Int) {
+//     result += 1
+//     if cnt == 1 { move.append("\(from) \(to)") }
+//     else {
+//         hanoi(from: from, to: 6-from-to, cnt: cnt - 1)
+//         move.append("\(from) \(to)")
+//         hanoi(from: 6-from-to, to: to, cnt: cnt - 1)
+//     }
+// }
