4cd9860532fc99fe8d6aac279fb7645837c5367e
yunkyung-Hwang <61379671+yunkyung-Hwang@users.noreply.github.com>
2022-09-22 23:30:11 +0900

    Dictionary

--- 10816_숫자 카드2.swift
+++ 10816_숫자 카드2.swift
@@ -4,29 +4,17 @@ let n = Int(readLine()!)!
 var card = readLine()!.split(separator: " ").map { Int($0)! }
 let m = Int(readLine()!)!
 var checkList = readLine()!.split(separator: " ").map { Int($0)! }
-var checkTmp = checkList
-var result: [Int: Int] = [:]
-var idx1 = 0
-var idx2 = 0
+var dic: [Int: Int] = [:]
+var result = [Int](repeating: 0, count: m)
 
 card.sort()
-checkList.sort()
+
+for card in card {
+    dic[card] = dic.keys.contains(card) ? dic[card]! + 1 : 1
+}
 
 for i in 0..<m {
-    result[checkList[i]] = 0
+    result[i] = dic.keys.contains(checkList[i]) ? dic[checkList[i]]! : 0
 }
 
-while idx1 < n && idx2 < m {
-    if card[idx1] == checkList[idx2] {
-        result[checkList[idx2]]! += 1
-        idx1 += 1
-    } else if card[idx1] > checkList[idx2] {
-        idx2 += 1
-    } else if card[idx1] < checkList[idx2] {
-        idx1 += 1
-    }
-}
-
-for int in checkTmp {
-    print(result[int]!, terminator: " ")
-}
+print(result.map { String($0) }.joined(separator: " "))
