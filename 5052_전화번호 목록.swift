993fd458bcb0fba669ecd0e5520f109dfdae7d0d
yunkyung-Hwang <61379671+yunkyung-Hwang@users.noreply.github.com>
2022-10-31 21:51:19 +0900

    문자열

--- 5052_전화번호 목록.swift
+++ 5052_전화번호 목록.swift
@@ -1,26 +1,51 @@
+// 풀이 2)
 import Foundation
 
 let t = Int(readLine()!)!
 for _ in 0..<t {
     let n = Int(readLine()!)!
     var phoneBook = [String](repeating: "", count: n)
-    var set = Set<String>()
     
     for i in 0..<n {
         phoneBook[i] = readLine()!
-        set.insert(phoneBook[i])
     }
-    
+    phoneBook.sort()
     print(calculate() ? "YES" : "NO")
-    
+
     func calculate() -> Bool {
-        for number in phoneBook {
-            for j in 1..<number.count {
-                if set.contains(String(number.prefix(j))) {
-                    return false
-                }
+        for i in 1..<n {
+            if phoneBook[i].hasPrefix(phoneBook[i-1]) {
+                return false
             }
         }
         return true
     }
 }
+
+// 풀이 1)
+// import Foundation
+
+// let t = Int(readLine()!)!
+// for _ in 0..<t {
+//     let n = Int(readLine()!)!
+//     var phoneBook = [String](repeating: "", count: n)
+//     var set = Set<String>()
+    
+//     for i in 0..<n {
+//         phoneBook[i] = readLine()!
+//         set.insert(phoneBook[i])
+//     }
+    
+//     print(calculate() ? "YES" : "NO")
+    
+//     func calculate() -> Bool {
+//         for number in phoneBook {
+//             for j in 1..<number.count {
+//                 if set.contains(String(number.prefix(j))) {
+//                     return false
+//                 }
+//             }
+//         }
+//         return true
+//     }
+// }
